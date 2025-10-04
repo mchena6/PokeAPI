ce513d54dbb1b2c69019a4f8775adad31779a3b3
mchena <m.chena@itecriocuarto.org.ar>
2025-10-03 23:05:15 -0300

    Lista de pokemones agregada

--- PokeAPI/src/app.jsx
+++ PokeAPI/src/app.jsx
@@ -1,43 +1,34 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './app.css'
 
+
 export function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  
+  // useEffect para cargar datos al montar el componente
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+    // Convertir respuesta a JSON
+    .then(response => response.json())
+    // Guardar datos en el estado
+    .then(data => setData(data))
+  }, []);
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>POKE API</h1>
+      <div className='card'></div>
+      <ul>
+        {/* Renderizar lista de pokemones */}
+        {data?.results.map(pokemon => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>     
+  );
 }
+
+
+
